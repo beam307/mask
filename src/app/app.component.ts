ae851dd1808599033de98e9141c333f430969d83
jerry <jerry@un7qi3.co>
2020-03-13 01:11:24 +0900

    디자인 수정 / 마커 => 오버레이로 수정 / 주소 검색기능 추가

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component } from '@angular/core';
+import { AfterViewInit, Component, ViewEncapsulation } from '@angular/core';
 import { MaskService } from './service/mask.service';
 
 declare var kakao;
@@ -6,18 +6,20 @@ declare var kakao;
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class AppComponent implements AfterViewInit {
 
   public map;
   public meter;
+  public customOverlay;
   public geocoder;
   public zoomControl;
   public markers = [];
   public options = {
     center: new kakao.maps.LatLng(37.428707, 127.151292),
-    level: 5
+    level: 3
   };
 
   constructor(private maskService: MaskService) {
@@ -27,74 +29,92 @@ export class AppComponent implements AfterViewInit {
     let container = document.getElementById('map');
 
     this.map = new kakao.maps.Map(container, this.options);
-    this.map.setMaxLevel(10);
+    this.map.setMaxLevel(9);
+    this.customOverlay = new kakao.maps.CustomOverlay({zIndex: 2});
     this.geocoder = new kakao.maps.services.Geocoder();
-    this.searchAddrFromCoords(this.map.getCenter(), this.displayCenterInfo.bind(this));
 
     this.zoomControl = new kakao.maps.ZoomControl();
     this.map.addControl(this.zoomControl, kakao.maps.ControlPosition.RIGHT);
 
+    this.displayCenterInfo(this.map.getCenter());
     this.eventListener();
 
   }
 
   eventListener() {
     kakao.maps.event.addListener(this.map, 'idle', () => {
+      this.displayCenterInfo(this.map.getCenter());
+    });
 
-      this.searchAddrFromCoords(this.map.getCenter(), this.displayCenterInfo.bind(this));
+  }
+
+  displayCenterInfo(result) {
+    let level = this.map.getLevel();
+    this.meter = level > 5 ? 972: 12 * Math.pow(level + 4, 2);
+    this.callAPI(result.getLat(), result.getLng());
+  }
+
+  callAPI(lng, lat) {
+    this.markers.forEach(m => m.setMap(null));
+    this.markers = [];
+    this.maskService.around(lng, lat, this.meter)
+      .then(r => r.stores.forEach(s => this.addMarker(s)))
+      .catch(e => {
+        console.log(e);
+      });
+  }
+
+  addMarker(info) {
+    let content = document.createElement('div');
+    content.className = 'info';
+    content.innerHTML = `
+      <div class="marker" style="background: ${ this.color(info.remain_stat) }">
+      </div>
+    `;
+    let marker = new kakao.maps.CustomOverlay({
+      map: this.map,
+      content: content,
+      position: new kakao.maps.LatLng(info.lat, info.lng)
+    });
+    this.markers.push(marker);
+    marker.setMap(this.map);
+
+    content.addEventListener('click', () => {
+      let content = `
+        <div class="name">
+            <p class="title">이름 : ${ info.name || '정보없음' }</p>
+            <p>주소 : ${ info.addr || '정보없음' }</p>
+            <p>입고날짜 : ${ info.stock_at || '정보없음' }</p>
+            <p>업데이트날짜 : ${ info.created_at || '정보없음' }</p>
+        </div>
+      `;
+      this.customOverlay.setContent(content);
+      this.customOverlay.setPosition(new kakao.maps.LatLng(info.lat, info.lng));
+      this.customOverlay.setMap(this.map);
     });
   }
 
-  searchAddrFromCoords(coords, callback) {
-    this.geocoder.coord2RegionCode(coords.getLng(), coords.getLat(), callback);
-  }
-
-  displayCenterInfo(result, status) {
-    if (status === kakao.maps.services.Status.OK) {
-      let level = this.map.getLevel();
-      this.meter = 1500 * Math.pow(2, level - 5);
-      this.markers.forEach(m => {
-        m.setMap(null);
-      });
-      this.markers = [];
-      this.maskService.around(result[0].y, result[0].x, this.meter)
-        .then(r => {
-          r.stores.forEach(s => {
-            let marker = new kakao.maps.Marker({
-              map: this.map,
-              position: new kakao.maps.LatLng(s.lat, s.lng)
-            });
-            let infowindow = new kakao.maps.InfoWindow({
-              content: `<div>
-                            <p>${ s.name }</p>
-                            <p>${ s.remain_stat }</p>
-                            <p>${ s.stock_at }</p>
-                        </div>`
-            });
-            this.markers.push(marker);
-            kakao.maps.event.addListener(marker, 'mouseover', this.makeOverListener(this.map, marker, infowindow));
-            kakao.maps.event.addListener(marker, 'mouseout', this.makeOutListener(infowindow));
-          });
-        })
-        .catch(e => {
-          console.log(e);
-        });
+  color(status?) {
+    if (status == 'plenty') {
+      return '#388e3c';
+    } else if (status == 'some') {
+      return '#ffa000';
+    } else if (status == 'few') {
+      return '#d32f2f';
+    } else {
+      return '#616161';
     }
-
-    // empty 회색, few 빨강, ,some, 노랑 plenty 녹색
   }
 
-  makeOverListener(map, marker, infowindow) {
-    return () => {
-      infowindow.open(map, marker);
-    };
+  search(event) {
+    this.geocoder.addressSearch(event, (result, status) => {
+      if (status === kakao.maps.services.Status.OK) {
+        this.map.setCenter(new kakao.maps.LatLng(result[0].y, result[0].x));
+        this.map.setLevel(3);
+      }
+    });
   }
 
-  makeOutListener(infowindow) {
-    return () => {
-      infowindow.close();
-    };
-  }
 
   zoomIn() {
     this.map.setLevel(this.map.getLevel() - 1);
